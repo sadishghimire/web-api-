2e4838bd638235282ca7f80cf00203975710916c
sadish <sadish10.sg@gmail.com>
2025-07-26 12:45:31 +0545

    Add project files.

---
+++ EmployeesAdminPortal/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using EmployeesAdminPortal.Models.Entities;
+using Microsoft.EntityFrameworkCore;
+namespace EmployeesAdminPortal.Data
+{
+    public class ApplicationDbContext:DbContext
+    {
+        public ApplicationDbContext(DbContextOptions options):base(options)
+        {
+
+        }
+        public DbSet<Employee> Employees { get; set; }
+
+    }
+}
